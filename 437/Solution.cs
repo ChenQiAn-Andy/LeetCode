cea4fcafd04abadf1796c361d4a9a75885bd787f
Andy Chen <qcenzo@gmail.com>
2022-07-03 16:08:38 +0800

    #437. 路径总和 III

---
+++ 437/Solution.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCode._437
+{
+    public class Solution
+    {
+        int paths;
+        void Check(TreeNode node, int sum)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            if (node.val == sum)
+            {
+                ++paths;
+            }
+            sum -= node.val;
+            Check(node.left, sum);
+            Check(node.right, sum);
+        }
+
+        void Traverse(TreeNode node, int sum)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            Check(node, sum);
+            Traverse(node.left, sum);
+            Traverse(node.right, sum);
+        }
+
+        public int PathSum(TreeNode root, int targetSum)
+        {
+            Traverse(root, targetSum);
+            return paths;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using LeetCode._437;
+
+using (SolutionWrapper wrapper = new SolutionWrapper437("../../../437/Test cases.txt",2))
+{
+    wrapper.Log();
+}
